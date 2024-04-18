62a4ddf377690d1f8e1afcbece24d35336f2eb65
shimaa <shimaa1234maa@gmail.com>
2024-04-18 19:02:24 +0200

    added

--- src/components/LandingPage/NavDropDowen.jsx
+++ src/components/LandingPage/NavDropDowen.jsx
@@ -1,44 +1,39 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 export default function NavDropDowen() {
   return (
     <div className="flex">
-        
-    <div className=" w-[200px] h-[200px] bg-landingBg  xl:hidden block shadow-lg text-yellowAcc overflow-auto text-[20px] rounded-xl fixed top-[100px] right-[50px]">
-      <ul>
+      <div className=" w-[200px] h-[200px] bg-landingBg  xl:hidden block shadow-lg text-yellowAcc overflow-auto text-[20px] rounded-xl fixed top-[100px] right-[50px]">
+        <ul>
           <Link to="/">
-              {" "}
-              <li className="hover:bg-greenAcc py-2 px-5 text-greenAcc font-tinos  rounded-xl font-bold hover:px-8 hover:text-white">
-                  Home
-              </li>
+            {" "}
+            <li className="hover:bg-greenAcc py-2 px-5 text-greenAcc font-tinos  rounded-xl font-bold hover:px-8 hover:text-white">
+              Home
+            </li>
           </Link>
-
 
           <Link to="/about">
-              {" "}
-              <li className="hover:bg-greenAcc py-2 px-5 text-greenAcc font-tinos  rounded-xl font-bold hover:px-8 hover:text-white">
-                  About Us
-              </li>
+            {" "}
+            <li className="hover:bg-greenAcc py-2 px-5 text-greenAcc font-tinos  rounded-xl font-bold hover:px-8 hover:text-white">
+              About Us
+            </li>
           </Link>
 
-          
-          <Link to="/servise">
-              {" "}
-              <li className="hover:bg-greenAcc py-2 px-5 text-greenAcc font-tinos  rounded-xl font-bold hover:px-8 hover:text-white">
-                  Service
-              </li>
+          <Link to="/service">
+            {" "}
+            <li className="hover:bg-greenAcc py-2 px-5 text-greenAcc font-tinos  rounded-xl font-bold hover:px-8 hover:text-white">
+              Service
+            </li>
           </Link>
           <Link to="/passform">
-              {" "}
-              <li className="hover:bg-greenAcc py-2 px-5 text-greenAcc font-tinos  rounded-xl font-bold hover:px-8 hover:text-white">
-                  passport
-              </li>
+            {" "}
+            <li className="hover:bg-greenAcc py-2 px-5 text-greenAcc font-tinos  rounded-xl font-bold hover:px-8 hover:text-white">
+              passport
+            </li>
           </Link>
-          
-        
-      </ul>
-      {/* <Link to="/login" ><li className='"hover:bg-greenAcc py-2 px-5 text-greenAcc font-tinos  rounded-xl font-bold hover:px-8 hover:text-white'>Sign in</li> </Link> */}
-  </div>
-</div>
-  )
+        </ul>
+        {/* <Link to="/login" ><li className='"hover:bg-greenAcc py-2 px-5 text-greenAcc font-tinos  rounded-xl font-bold hover:px-8 hover:text-white'>Sign in</li> </Link> */}
+      </div>
+    </div>
+  );
 }
