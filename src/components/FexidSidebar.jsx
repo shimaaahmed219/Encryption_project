c09f0b3be2a25d028ca1dc372f43930884428f7c
shimaa <shimaa1234maa@gmail.com>
2024-04-18 19:45:25 +0200

    add

--- src/components/FexidSidebar.jsx
+++ src/components/FexidSidebar.jsx
@@ -26,9 +26,9 @@ export default function FexidSidebar() {
     { id: 7, name: "Encryption file", href: "/encryption", src: Encrypted },
     { id: 8, name: "Decrypt file", href: "/Decrypt", src: decimg },
     { id: 9, name: "Employees", href: "/employee", src: employee },
-    { id: 10, name: "forginEmployee", href: "/forginEmployee", src: users },
-    { id: 11, name: "recruitment", href: "/recruitment", src: Settingsicon },
-    { id: 12, name: "recruitment", href: "/recruitment", src: Settingsicon },
+    { id: 10, name: "forginEmployee", href: "/ForgenEmployee", src: users },
+    { id: 11, name: "recruitment", href: "/recruitmentArea", src: Settingsicon },
+    // { id: 12, name: "recruitment", href: "/recruitment", src: Settingsicon },
   ];
 
   const [data, setData] = useState({});
