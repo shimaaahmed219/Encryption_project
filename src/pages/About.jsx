0ace31e873aac6a0e6f899813692f3c63c4e65d7
shimaa <shimaa1234maa@gmail.com>
2024-04-20 12:33:13 +0200

    about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -44,10 +44,10 @@ export default function About() {
           </div>
           {/*  */}
     
-          <div className="h-[360px]  w-full flex md:flex-row flex-col lg:my-[100px] sm:my-[-50px]   justify-center gap-x-[70px] items-center">
+          <div className="h-[360px]  w-full flex md:flex-row flex-col lg:my-[100px] sm:my-[-50px]    justify-center gap-x-[70px] items-center">
             <div className="">
-              <h1 className="md:text-[48px] ml-10 sm:text-[30] text-[25px] font-tinos sm:mt-0 mt-[-300px] font-bold">Our mission</h1>
-              <p className="lg:w-[475px] md:w-[350px] sm:w-[475px] w-[350px] ml-10 md:mb-0 mb-[50px] m-auto lg:text-[24px] md:[20px] font-normal text-greenAcc leading-8 font-roboto">
+              <h1 className="md:text-[48px] ml-10 sm:text-[30] text-[25px] font-tinos sm:mt-0 mt-[-270px] sm:text-left  font-bold">Our mission</h1>
+              <p className="lg:w-[475px] md:w-[350px] sm:pr-0 sm:w-[475px] w-[250px] pr-6 ml-10 md:mb-0 mb-[50px] m-auto lg:text-[24px] md:text-[20px] text-[15px] font-normal text-greenAcc leading-8 font-roboto">
                 text ever since when an unknown printer took a galley of type and
                 scrambled it to make a type specimen book. It has survived not only
                 five centuries, but also the leap into electronic typesetting,
@@ -62,16 +62,16 @@ export default function About() {
     
           {/*  */}
     
-          <div className=" grid xl:grid-cols-3 md:grid-cols-2 grid-cols-1 w-5/6 justify-between m-auto text-center my-[200px]">
+          <div className=" grid xl:grid-cols-3 md:grid-cols-2 grid-cols-1 w-5/6 justify-between m-auto text-center md:my-[200px] my-[50px]">
             {technology.map((item) => (
-              <div key={item.id} className="">
+              <div key={item.id} className="w-full my-10">
                 <div className="bg-yellowAcc w-[155px] h-[155px] m-auto  rounded-full flex justify-center items-center">
                   <img className="w-[73px] h-[91px]" src={item.src} />
                 </div>
-                <h1 className="text-[28px] leading-8 text-greenAcc my-3 font-tinos font-bold">
+                <h1 className="sm:text-[28px] text-[24px] leading-8 text-greenAcc my-3 font-tinos font-bold">
                   {item.name}
                 </h1>
-                <p className="w-[312px] m-auto mb-10 leading-7 font-normal text-[24px] font-roboto text-p">
+                <p className="sm:w-[312px] m-auto mb-10 leading-7 font-normal sm:text-[24px] text-[20px] font-roboto text-p">
                   {item.des}
                 </p>
               </div>
