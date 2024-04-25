95e8332a4272309ce969ae826676f53f4e933aea
shimaa <shimaa1234maa@gmail.com>
2024-04-25 21:10:31 +0200

    handilAddForm

--- src/pages/AddEmployee.jsx
+++ src/pages/AddEmployee.jsx
@@ -2,46 +2,44 @@ import AddEmployeeForm from "../components/AddEmployee/AddEmployeeForm";
 import FexidSidebar from "../components/FexidSidebar";
 import Nav from "../components/Nav";
 
-
+import icon from "../assets/navImg/Group (1).svg";
 export default function AddEmployee() {
   return (
-   
-//     <div className='min-h-screen bg-bg pb-20 '>
-//     <div className='flex  flex-row'>
-     
-//         <div className='lg:block hidden'>
-//       <FexidSidebar/>
-       
-//         </div>
-       
-//         <div className='w-full h-full'>
-//         <Nav/>
-//         <hr className='w-full h-[0.15rem] bg-greenAcc my-2'/>
-//         <div className='w-full h-full p-7'>
-//         <h1 className={` font-roboto ms-[50px] text-[26px] lg:ml-[400px]  capitalize text-greenAcc`}>Add Employee </h1>
-//         </div>
-//         <div></div>
-//        <AddEmployeeForm/>
-//         </div>
-//     </div>
-  
+    // </div>
+    <div className="min-h-screen bg-bg">
+      <div className="xl:flex  flex-row">
+        <div className=" w-[26%] min-h-screen xl:block hidden">
+          <FexidSidebar />
+        </div>
+        <div className=" py-2 w-full h-full">
+          <div className="xl:hidden block">
+            <Nav />
+          </div>
+          <div className="xl:block hidden">
+            <div className="flex row w-full">
+              <div className="ms-auto flex">
+                <h1
+                  className={` font-tinos text-[26px] text-greenAcc mt-2 mr-3  text-xl`}
+                >
+                  <span className="text-yellowAcc text-[22px]">E-</span>passport
+                </h1>
 
+                <img
+                  src={icon}
+                  alt="enc-img"
+                  className="ms-auto md:block hidden transition mr-[90px] ease-in-out delay-150 duration-700 "
+                />
+              </div>
+            </div>
+          </div>
 
-// </div>
-<div className="min-h-screen bg-bg">
-<div className="xl:flex  flex-row">
-  <div className=" w-[26%] min-h-screen xl:block hidden">
-    <FexidSidebar />
-  </div>
-  <div className=" py-2 w-full h-full">
-    <Nav />
-    <hr className="w-full h-[0.15rem] bg-greenAcc my-2" />
-    <div className="w-full h-full flex justify-center items-center p-1">
-      {/* <h1 className={`${rob.className} ms-[50px]  text-[26px]  capitalize text-greenAcc`}>Edit Employee </h1> */}
-      <AddEmployeeForm/>
+          <hr className="w-full h-[0.15rem] bg-greenAcc my-2" />
+          <div className="w-full h-full flex justify-center items-center p-1">
+            {/* <h1 className={`${rob.className} ms-[50px]  text-[26px]  capitalize text-greenAcc`}>Edit Employee </h1> */}
+            <AddEmployeeForm />
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-</div>
-  )
+  );
 }
--- src/pages/EditEmployee.jsx
+++ src/pages/EditEmployee.jsx
@@ -1,7 +1,7 @@
 import FexidSidebar from "../components/FexidSidebar";
 import Nav from "../components/Nav";
 import EditEmpForm from "../components/UpdateEmployee/EditEmpForm";
-
+import icon from "../assets/navImg/Group (1).svg";
 export default function EditeProfile() {
   return (
     <div className="min-h-screen bg-bg">
@@ -9,8 +9,28 @@ export default function EditeProfile() {
         <div className=" w-[26%] min-h-screen xl:block hidden">
           <FexidSidebar />
         </div>
+
         <div className=" py-2 w-full h-full">
-          <Nav />
+          <div className="xl:hidden block">
+            <Nav />
+          </div>
+          <div className="xl:block hidden">
+            <div className="flex row w-full">
+              <div className="ms-auto flex">
+                <h1
+                  className={` font-tinos text-[26px] text-greenAcc mt-2 mr-3  text-xl`}
+                >
+                  <span className="text-yellowAcc text-[22px]">E-</span>passport
+                </h1>
+
+                <img
+                  src={icon}
+                  alt="enc-img"
+                  className="ms-auto md:block hidden transition mr-[90px] ease-in-out delay-150 duration-700 "
+                />
+              </div>
+            </div>
+          </div>
           <hr className="w-full h-[0.15rem] bg-greenAcc my-2" />
           <div className="w-full h-full flex justify-center items-center p-1">
             {/* <h1 className={`${rob.className} ms-[50px]  text-[26px]  capitalize text-greenAcc`}>Edit Employee </h1> */}
